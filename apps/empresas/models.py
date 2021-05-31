2d487cacc6d639fb1fec9961281ba2ed0cb34a4a
max guntzel <max.guntzel@voffice.com.br>
2021-05-31 09:32:50 -0300

    added relation to tables

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Empresas(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome da Empresa')
 
-
+    def __str__(self):
+        return self.nome
