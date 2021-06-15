480b19e16a7d63f160cf4d13dd160dc633aaf2bd
max guntzel <max.guntzel@voffice.com.br>
2021-06-15 09:31:14 -0300

    added template core as home url

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'apps.departamentos',
     'apps.documentos',
     'apps.registro_hora_extra',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
