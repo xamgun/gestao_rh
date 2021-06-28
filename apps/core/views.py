bbaf43ad094f29da7afdd44ff5e562b0aafd3c5a
max guntzel <max.guntzel@voffice.com.br>
2021-06-28 08:20:36 -0300

    added empresas save

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from apps.funcionarios.models import Funcionario
 
 
 # Create your views here.
 @login_required
 def home(request):
-    return render(request, 'core/index.html')
+    data = {}
+    data['usuario'] = request.user
+    return render(request, 'core/index.html', data)
--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Empresas
+
 
 # Create your views here.
+class EmpresaCreate(CreateView):
+    model = Empresas
+    fields = ['nome']
