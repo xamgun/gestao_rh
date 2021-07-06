04dfcf205c0e4d7b21035a623363a4365c832f4b
max guntzel <max.guntzel@voffice.com.br>
2021-07-06 08:03:27 -0300

    attach user to empresa

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,6 @@
+import http
+
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Empresas
@@ -7,3 +10,10 @@ from .models import Empresas
 class EmpresaCreate(CreateView):
     model = Empresas
     fields = ['nome']
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('ok')
