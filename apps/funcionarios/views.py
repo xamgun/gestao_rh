62eae7023a2b0ef4bef5344703790aa3d2aec3e2
max guntzel <max.guntzel@voffice.com.br>
2021-07-08 09:48:04 -0300

    Funcionarios list

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Funcionario
 
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Olá')
+class FuncionariosList(ListView):
+    model = Funcionario
+
+
+
