a1a3f40216ae9a2b1686610ad2a8b4b93f960f15
max guntzel <max.guntzel@voffice.com.br>
2021-07-09 08:29:24 -0300

    Only Funcionarios from especific empresa

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -6,5 +6,9 @@ from .models import Funcionario
 class FuncionariosList(ListView):
     model = Funcionario
 
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
+
 
 
