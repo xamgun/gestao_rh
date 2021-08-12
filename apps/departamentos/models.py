6909aeeb3519003d842282d2c1b40967c48e164a
Max Guntzel <max.guntzel@sippulse.com>
2021-08-12 07:36:49 -0300

    Created Department list

--- apps/departamentos/models.py
+++ apps/departamentos/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from apps.empresas.models import Empresas
 
 
 # Create your models here.
 class Departamento(models.Model):
     nome = models.CharField(max_length=70)
+    empresa = models.ForeignKey(Empresas, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
+
+
--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,3 +1,12 @@
+from django.views.generic import ListView
+from .models import Departamento
+
 from django.shortcuts import render
 
 # Create your views here.
+class DepartamentosList(ListView):
+    model = Departamento
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Departamento.objects.filter(empresa=empresa_logada)
