ae29fdd3a96836d23727f081e194ef9601e13b19
max <max.guntzel@voffice.com.br>
2022-04-03 09:42:32 -0300

    finalizando crud

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     HoraExtraList,
     HoraExtraEdit,
-    HoraExtraDelete
+    HoraExtraDelete,
+    HoraExtraCreate
 )
 
 
@@ -10,5 +11,5 @@ urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
     path('editar/<int:pk>', HoraExtraEdit.as_view(), name='update_hora_extra'),
     path('delete/<int:pk>', HoraExtraDelete.as_view(), name='delete_hora_extra'),
-    # path('novo', FuncionariosNovo.as_view(), name='create_funcionarios'),
+    path('novo', HoraExtraCreate.as_view(), name='create_hora_extra'),
 ]
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -3,7 +3,8 @@ from pyexpat import model
 
 from django.urls import reverse_lazy
 from .models import RegistroHoraExtra
-from django.views.generic import (ListView, UpdateView, DeleteView)
+from .forms import RegistroHoraExtraForm
+from django.views.generic import (ListView, UpdateView, DeleteView, CreateView)
 
 # Create your views here.
 class HoraExtraList(ListView):
@@ -23,6 +24,18 @@ class HoraExtraEdit(UpdateView):
 class HoraExtraDelete(DeleteView):
     model = RegistroHoraExtra
     success_url = reverse_lazy('list_hora_extra')
+
+
+class HoraExtraCreate(CreateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+    success_url = reverse_lazy('list_hora_extra')
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraCreate, self).get_form_kwargs()
+        kwargs.update({'user' : self.request.user})
+        return kwargs
 
 
 
