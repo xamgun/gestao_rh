fbabd0e847e3feccd068c2511c42e25a5d5756da
xamgun <xamgun@gmail.com>
2022-05-02 07:22:19 -0300

    list only users from company

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -9,10 +9,14 @@ from django.views.generic import (ListView, UpdateView, DeleteView, CreateView)
 # Create your views here.
 class HoraExtraList(ListView):
     model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
 
-    def get_queryset(self):
-        empresa_logada = self.request.user.funcionario.empresa
-        return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+    success_url = reverse_lazy('list_hora_extra')
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraCreate, self).get_form_kwargs()
+        kwargs.update({'user' : self.request.user})
+        return kwargs
 
 
 class HoraExtraEdit(UpdateView):
